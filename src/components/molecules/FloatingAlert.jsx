7672c85d9d144dcad2f7d70aecad016360765fb0
Mostafa Mohamed <65525706+Mostafa4Abdelwahed@users.noreply.github.com>
2025-06-30 01:28:23 +0300

    handle size for floating data on mobile

--- src/components/molecules/FloatingAlert.jsx
+++ src/components/molecules/FloatingAlert.jsx
@@ -3,10 +3,10 @@ import { Icon } from "@iconify/react"
 function FloatingAlert({ icon="", text="", className="" }) {
     return (
         <div className={`flex items-center gap-2.5 ${className}`}>
-            <div className="bg-main p-3 rounded-full text-light text-3xl">
+            <div className="bg-main p-3 rounded-full text-light text-base lg:text-3xl">
                 <Icon icon={icon} />
             </div>
-            <div className="bg-white border border-gray-300/75 text-main text-base font-bold py-3 px-4 rounded-2xl">
+            <div className="bg-white border border-gray-300/75 text-main text-xs lg:text-base font-bold lg:py-3 lg:px-4 p-2.5 rounded-2xl">
                 {text}
             </div>
         </div>
